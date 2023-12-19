33a44565a855142267b9599738c73ea4f9a73b54
Himanshu Awasthi <himan.7525@gmail.com>
2023-12-19 10:40:26 +0530

    let var const

---
+++ letVarConst.js
@@ -0,0 +1,10 @@
+const accountId = 123456;
+//cannot be changed
+let accountEmail = "HELLO@EXAMPLE.COM";
+//can be chaged
+var accountPass = 23131321;
+//can be chaned
+accountCity = "varanasi";
+//don't do this
+
+console.table([accountEmail, accountId, accountPass, accountCity]);
